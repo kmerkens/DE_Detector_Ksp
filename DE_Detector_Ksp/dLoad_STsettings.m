1be2be454efc8f139949fb7a9fa6ff478be00d7b
kmerkens <Karlina.Merkens@PICSH25.pifsc.gov>
2015-03-26 09:51:58 -1000

    Click detector adapted to target Kogia spp clicks
    Adaptation of kfrasier's DE_Detector

---
+++ DE_Detector_Ksp/dLoad_STsettings.m
@@ -0,0 +1,22 @@
+function parametersST = dLoad_STsettings
+
+% Assign short term detector settings
+
+parametersST.buff = 500; % # of buffer samples to add on either side of area of interest
+parametersST.chan = 1; % which channel do you want to look at?
+
+parametersST.fRanges = [50000 99000]; 
+parametersST.thresholds = 13500; % Amplitude threshold in counts. 
+% For predictability, keep this consistent between low and hi res steps.
+
+parametersST.frameLengthSec = .01; %Used for calculating fft size
+parametersST.overlap = .50; % fft overlap
+parametersST.REWavExt = '(\.x)?\.wav';%  expression to match .wav or .x.wav
+
+% if you're using wav files that have a time stamp in the name, put a
+% regular expression for extracting that here:
+parametersST.DateRE = '_(\d*)_(\d*)';
+% mine look like "filename_20110901_234905.wav" 
+% ie "*_yyyymmdd_HHMMSS.wav"
+
+
---
+++ DE_Detector_Ksp/de_detector.m
@@ -0,0 +1,89 @@
+function de_detector
+% This is the starting point for a simplified detector based on Marie Roche's  
+% teager energy detector. It includes ideas/code snips from Simone, 
+% and calls functions from triton. 
+% The goal of this detector is to have predictable performance, 
+% for use with model-based density estimation efforts. To accomplish this,
+% it uses a simple energy threshold to identify clicks, thereby reducing
+% the impact of changing noise conditions on detectability. 
+
+% Known issue: Prop cavitation noise often makes it through detector and
+% classifier steps.
+
+% The low and hi-res detection passes still happen, but no teager energy 
+% is used.
+
+% All input parameters are contained within two separate scripts:
+%   dLoad_STsettings : settings for low res detector
+%   dLoad_HRsettings : settings for hi res detector
+% See those files for info on settings.
+
+% clearvars
+close all
+fclose all;
+
+tic
+
+% Set transfer function location
+%tfFullFile = 'E:\Code\TF_files\604_100614\604_100614_invSensit.tf';
+tfFullFile = 'H:\Cetacean Research Program\HARP\TF_files\695_121203_invSensit.tf';
+%tfFullFile = 'C:\Users\Karlina.Merkens\Documents\HARPTFfiles\400_series\406_070717\406_070717_invSensit.tf';
+
+% Note, if you don't have a tranfer function just use:
+% tfFullFile = [];
+
+
+% Location of base directory containing directories of files to be analyzed
+%baseDir = 'I:\GofMXArraySpRecs\Sb';
+baseDir = 'F:\';
+
+% Name of the deployment. This should be the first few characters in the 
+% directory(ies) you want to look in you want to look at. For now,
+% directory hierarchy is expected to be: basedir>depl*>*.x.wav
+% TODO: implement recursive directory search for more flexibility.
+%depl = 'GofMX_';
+depl = 'Tinian';
+
+
+% Set flags indicating which routines to run. 
+lowResDet =1; %run short time detector.
+highResDet = 1; %run high res detector
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[metaDir] = dBuild_dirs(baseDir);
+inDisk = fileparts(baseDir(1:3));
+
+% Build list of (x)wav names in the base directory.
+% Right now only wav and xwav files are looked for.
+[detFiles]= dFind_xwavs(baseDir,depl); % doesn't read in manual detection 
+% files in this version, but this can be added pretty easily, using an
+% older version.
+
+viewPath = {metaDir, baseDir};
+[fullFiles,fullLabels] = get_fileset(baseDir,metaDir,detFiles); % returns a list of files to scan through
+% profile on
+% profile clear
+if ~isempty(detFiles)
+    % Short time detector
+    if lowResDet == 1
+        % load settings
+        parametersST = dLoad_STsettings;
+        % run detector
+        dtST_batch(baseDir,detFiles,parametersST,viewPath);
+        display('Done with low-res detector')
+        toc
+    end
+    
+    % High res detector
+    if highResDet == 1
+        % load settings
+        parametersHR = dLoad_HRsettings;
+        % run detector
+        dHighres_click_batch(fullFiles,fullLabels,baseDir,parametersHR,viewPath,tfFullFile)
+        display('Done with high-res detector')
+    end
+end
+
+toc
+% profile viewer
+% profile off
